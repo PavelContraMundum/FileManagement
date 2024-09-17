6b2042dce8758ca1243a4339fed66ab8dff5372d
PavelContraMundum <pavel.sticka@gmail.com>
2024-09-17 19:29:12 +0200

    skrývání panelu, úprava mainpage

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './MainPage.css';
+import documentsPic from './images/documents.png'
 
 function MainPage() {
     return (
         <div className="main-page">
-            <h1>Welcome to MainPage</h1>
+            <div className='main-header'>
+            <h1 >Welcome to MainPage</h1>
+            </div>
+            
+
+            <div className='main-pic'>
+                <img src={documentsPic} alt="My image" />
+            </div>
         </div>
     );
 }
